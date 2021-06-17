bf98001e16d6445e7c76964fcac6177eddcc2f5e
timismall <timileyinakintayo@gmail.com>
2021-06-17 11:05:34 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:'#181E1E',
+        secondary: '#A4703E',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
